8c6eef8ec39e099aa9233a45a91f3d2669835fa1
Sarwa85 <70193705+Sarwa85@users.noreply.github.com>
2024-02-22 22:31:31 +0100

    Update base.py

--- flaskr/base.py
+++ flaskr/base.py
@@ -2,6 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine("sqlite:///data/slowotlok-flask.db")
+engine = create_engine("sqlite:////data/slowotlok-flask.db")
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
